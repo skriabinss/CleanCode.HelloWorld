817ab65eebac2f990feceb76caec2c8d3c1e0f0b
SerhiiSkriabin <skriabinss@gmail.com>
2022-02-07 15:30:39 +0200

    Added answers

--- CleanCode.HelloWorld.CLI/Program.cs
+++ CleanCode.HelloWorld.CLI/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("1) Программирование для меня это хобби");
+Console.WriteLine("2) Я хочу стать программистом, чтобы писать крутые приложения");
+
